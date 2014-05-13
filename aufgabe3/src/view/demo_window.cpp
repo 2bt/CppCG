21f942d59d2b49273959baf62d2cbe45e8f76680
twobit <twobit@lavabit.com>
2014-05-14 01:25:19 +0200

    shorten

--- aufgabe3/src/view/demo_window.cpp
+++ aufgabe3/src/view/demo_window.cpp
@@ -8,7 +8,6 @@ bool DemoWindow::display() {
 	glClearColor(0.1, 0.2, 0.3, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(30, width() / static_cast<double>(height()), 2, 20);
@@ -18,8 +17,7 @@ bool DemoWindow::display() {
 
 	auto dur = start_time - std::chrono::steady_clock::now();
 	auto s = std::chrono::duration_cast<std::chrono::duration<float>>(dur);
-	float ang = 360 / 5 * s.count();
-	glRotatef(ang, 0, 0, 1);
+	glRotatef(360 / 5 * s.count(), 0, 0, 1);
 
 	glColor3f(0.75, 0.75, 0.75);
 	glBegin(GL_LINES);
